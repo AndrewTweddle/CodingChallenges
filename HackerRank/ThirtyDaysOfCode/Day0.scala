79fa82fe0dd1386f4b69f3d76b3ebc8dd76ed935
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2016-08-04 13:48:49 -0400

    Hacker Rank: 30 day challenge: day 0 (Scala)

---
+++ HackerRank/ThirtyDaysOfCode/Day0.scala
@@ -0,0 +1,9 @@
+import scala.io._
+
+object Solution {
+    def main(args: Array[String]): Unit = {
+        val inputString = StdIn.readLine()
+        println("Hello, World.")
+        println(inputString)
+    }
+}
