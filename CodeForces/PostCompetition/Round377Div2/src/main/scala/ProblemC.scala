cd68c5544bb252d490382100df3e96ecde89dc17
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2016-10-17 15:00:04 -0400

    CF: Post-competition: #377 div 2: C: fix logic bug
    - Missed that b,s,d with b == s > d can be solved with (s, b, d)
    - This solution now passes the tests

--- CodeForces/PostCompetition/Round377Div2/src/main/scala/ProblemC.scala
+++ CodeForces/PostCompetition/Round377Div2/src/main/scala/ProblemC.scala
@@ -42,11 +42,8 @@ object ProblemC {
   def solve(b: Long, d: Long, s: Long): Long = {
     // rebase time, so that at least as many of first meal as any other, and call that breakfast...
     if (d > b && d >= s) solve(d, s, b)  // Measure from before dinner on the first day
-    else if (s > d && s > b) solve(s, b, d)  // Measure from before supper on the first day
-    else {
-      if (s == b) b - d  // Stay for full last day
-      else math.max(b - d - 1, 0) + b - s - 1
-        // don't count last dinner and supper as missed, but beware that d could equal b
-    }
+    else if (s > d && s >= b) solve(s, b, d)  // Measure from before supper on the first day
+    else math.max(b - d - 1, 0) + math.max(b - s - 1, 0)
+        // don't count last dinner and/or supper as missed, but beware that b, d and s could be equal
   }
 }
