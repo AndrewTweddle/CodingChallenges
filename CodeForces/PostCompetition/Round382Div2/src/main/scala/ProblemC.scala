57195cd2c7afe8fcd6f69d75757125eb3c18321b
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2016-12-03 14:23:53 -0500

    CF #382: div 2: post-competition: C: timing out
      - previous algorithm failed as it followed a greedy approach
      - modified to brute force the initial rounds
      - gets further (so algorithm better), but times out
      - the right algorithm uses Fibonacci numbers
    
      - TODO: submit a new solution using Fibonacci

--- CodeForces/PostCompetition/Round382Div2/src/main/scala/ProblemC.scala
+++ CodeForces/PostCompetition/Round382Div2/src/main/scala/ProblemC.scala
@@ -35,24 +35,74 @@ object ProblemC {
 
   def solve(n: Long): Long = {
     @tailrec
-    def loop(minGames: Int, counts: List[Long]): Long = {
-      val fullList: List[Long] = List.fill[Long](minGames)(0L) ++ counts
-      println(fullList.mkString(", "))
-      counts match {
+    def loop(minGames: Int, countsByNumberOfWins: List[Long]): Option[Long] = {
+      /*
+      if (countsByNumberOfWins.head != 0) {
+        val fullList: List[Long] = List.fill[Long](minGames)(0L) ++ countsByNumberOfWins
+        println(fullList.mkString(", "))
+      }
+      */
+      countsByNumberOfWins match {
         case 0 :: tl           => loop(minGames + 1, tl)
-        case 1 :: Nil          => minGames
-        case a :: Nil          => loop(minGames, List(a - 2, 1))
-        case 1 :: 1 :: Nil     => minGames + 2
-        case 1 :: 1 :: c :: tl => loop(minGames + 2, (c + 1) :: tl)
-        case 2 :: 1 :: tl      => loop(minGames + 1, 2 :: tl)  // Don't leave an "orphaned" player
-        case a :: 0 :: tl      => loop(minGames, (a - 2) :: 1l :: tl)
-        case a :: b :: Nil     => loop(minGames, List(a - 1, b - 1, 1))
-        case a :: b :: c :: tl => loop(minGames, (a - 1) :: (b - 1) :: (c + 1) :: tl)
-        case Nil               => 0
+        case 1 :: Nil          => Some(minGames)
+        case 1 :: 1 :: Nil     => loop(minGames + 2, 1L :: Nil)
+
+        // Only allow uneven matches (where one player has played an extra game):
+        case a :: Nil          => None
+        case a :: 0 :: tl      => None
+
+        // Reject if there are more "a" players than "b", so that we don't have to decide how many to eliminate
+        // (since that decision must be made up-front):
+        case a :: b :: tl if a > b  => None
+
+          // Have all the "a" players lost to that many "b" players (who increase their number of wins):
+        case a :: b :: Nil     => loop(minGames + 1, List(b - a, a))
+        case a :: b :: c :: tl => loop(minGames + 1, (b - a) :: (c + a) :: tl)
+
+        case _  => None
       }
     }
 
-    val maxGamesPlayed = loop(0, List(n))
+    def runTournament(matchesWhereBothPlayersHavePlayedNoGames: Long,
+                      playersWithNoGamesWhoWillLoseToAPlayerWithOneGame: Long): Long = {
+      val playersWithNoGamesWhoEliminateAPlayerWithOneWin =
+        n - 2 * matchesWhereBothPlayersHavePlayedNoGames - playersWithNoGamesWhoWillLoseToAPlayerWithOneGame
+      if (matchesWhereBothPlayersHavePlayedNoGames == 0 && playersWithNoGamesWhoEliminateAPlayerWithOneWin > 0) {
+        0
+      } else {
+        /*
+        val msg =
+          s"""Attempt a tournament with $n players and the following initial results:
+           |  a) $matchesWhereBothPlayersHavePlayedNoGames matches where both players haven't played yet.
+           |  b) $playersWithNoGamesWhoEliminateAPlayerWithOneWin matches where a player with 0 games beats a player with 1 win.
+           |  c) $playersWithNoGamesWhoWillLoseToAPlayerWithOneGame  who haven't played yet
+           |     (but will lose to players with no games who will lose to a player with 1 game).
+         """
+        println(msg.stripMargin)
+        */
+        val playersByNumberOfWins =
+          List(playersWithNoGamesWhoWillLoseToAPlayerWithOneGame, matchesWhereBothPlayersHavePlayedNoGames)
+        val maxGamesOption = loop(0, playersByNumberOfWins)
+        val maxPlayed = maxGamesOption.getOrElse(0L)
+        /*
+        if ( maxGamesOption.isDefined) {
+          println(s"Max games played: $maxPlayed")
+        } else {
+          println("No solution found")
+        }
+        println()
+        */
+        maxPlayed
+      }
+    }
+
+    // Look for a solution where only in the first round do people with the same number of wins play each other.
+    // Thereafter expect all matches to be lopsided with the better player having more wins:
+    val tournaments = for {
+      numEvens <- 0L to n/2
+      numUnevens <- 0L to n - 2 * numEvens
+    } yield runTournament(numEvens, numUnevens)
+    val maxGamesPlayed = tournaments.max
     maxGamesPlayed
   }
 }
